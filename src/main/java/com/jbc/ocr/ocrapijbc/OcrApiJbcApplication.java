5e97a32196c7398e5d9bd5ebc173355a6140272a
gibers <chbaptis@hotmail.fr>
2019-04-28 14:08:51 +0200

    first c.

---
+++ src/main/java/com/jbc/ocr/ocrapijbc/OcrApiJbcApplication.java
@@ -0,0 +1,13 @@
+package com.jbc.ocr.ocrapijbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrApiJbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcrApiJbcApplication.class, args);
+    }
+
+}
